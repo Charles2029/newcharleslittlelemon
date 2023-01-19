bae352e25dbc383509496782434189bfa181a9d2
charles2028 <love98982016@gmail.com>
2023-01-20 01:26:26 +0800

    to remote

--- charleslittlelemon/src/App.js
+++ charleslittlelemon/src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Header";
+import React, { useState } from "react";
+import { TaskList } from "./components/TaskList";
 
 function App() {
   return (
@@ -11,6 +13,18 @@ function App() {
 
     </>
   );
+}
+
+
+export default function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, task: "Go shopping", done: true },
+    { id: 2, task: "Wash dishes", done: false },
+  ]);
+
+  return (
+    <TaskList tasks={tasks} />
+  )
 }
 
 export default App;
---
+++ charleslittlelemon/src/Nav.js
@@ -0,0 +1,6 @@
+
+
+<Routes> 
+    <Route path="/" element={<HomePage />}></Route>
+    <Route path="/booking" element={<BookingPage />}></Route>
+</Routes>
